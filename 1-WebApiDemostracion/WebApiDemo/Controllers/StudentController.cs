71a7f9faf10f94fcbfb6927f5288e7d8acba5ac7
FlorenciaPalazzo <florencia.e.palazzo@gmail.com>
2022-11-16 19:04:25 -0300

    first commit

---
+++ 1-WebApiDemostracion/WebApiDemo/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private List<Student> students()
+        {
+            List<Student> studentsLocal = new List<Student>()
+            {
+                new Student(){Id= 1, LastName="Pint" ,Name= "Gustavo"},
+                new Student(){Id= 2, LastName="Mayorga" ,Name= "Maria"},
+                new Student(){Id= 3, LastName="Alvarez" ,Name= "Julian"},
+            };
+            return studentsLocal;
+        }
+        //GET: /api/student/
+        [HttpGet]
+        public  IEnumerable<Student> Get()
+        {
+            return students();
+        }
+
+        //GET: /api/student/
+        [HttpGet("{id}")]
+        public Student Get( int id)
+        {
+            Student student = students().Find(x => x.Id == id);
+            return student;
+        }
+
+    }
+}
---
+++ 2-DemoEscuela/WebApiEscuela/Controllers/ProfesorController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using WebApiEscuela.Data;
+using WebApiEscuela.Models;
+
+namespace WebApiEscuela.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfesorController : ControllerBase
+    {
+        public DBEscuelaAPIContext Context { get; set; }
+        public ProfesorController(DBEscuelaAPIContext context)
+        {
+            this.Context = context;
+        }
+
+        /* métodos/acciones*/
+
+        //TRAER TODOS
+        [HttpGet]
+        public List<Profesor> Get()
+        {
+            //EF
+            List<Profesor> profesores = Context.Profesores.ToList();
+            return profesores;
+        }
+        //TRAER UNO
+        [HttpGet("{id}")]
+        public Profesor Get(int id)
+        {
+            //EF
+            Profesor profesor = Context.Profesores.Find(id);
+
+            return profesor;
+        }
+
+        //INSERTAR 
+        [HttpPost]
+        public ActionResult Post(Profesor profesor)
+        {
+            //EF -- memoria
+            Context.Profesores.Add(profesor);
+            //EF - Guardar en la DB
+            Context.SaveChanges();
+
+            return Ok();
+        }
+
+        //MODIFICAR
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Profesor profesor)
+        {
+            if (id != profesor.Id)
+            {
+                return BadRequest();
+            }
+
+            //EF para modificar en la DB
+            Context.Entry(profesor).State = EntityState.Modified;
+            Context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Profesor> Delete(int id)
+        {
+            //EF
+            var profesor = Context.Profesores.Find(id);
+
+            if (profesor == null)
+            {
+                return NotFound();
+            }
+
+            //EF
+            Context.Profesores.Remove(profesor);
+            Context.SaveChanges();
+
+            return profesor;
+
+        }
+
+    }
+}
