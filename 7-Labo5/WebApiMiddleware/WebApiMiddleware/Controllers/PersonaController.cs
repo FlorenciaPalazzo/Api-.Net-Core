9cb774859a1f01229310bb34db80c69260101414
FlorenciaPalazzo <florencia.e.palazzo@gmail.com>
2022-11-23 12:05:08 -0300

    lab 5-finalizado

---
+++ 7-Labo5/WebApiMiddleware/WebApiMiddleware/Controllers/PersonaController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiMiddleware.Models;
+
+namespace WebApiMiddleware.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize]
+    public class PersonaController : ControllerBase
+    {
+        private List<Persona> _listaPersonas = new List<Persona>();
+
+        public PersonaController()
+        {
+            _listaPersonas.Add(new Persona() {Id = 1, Nombre = "Juan", Apellido = "Spray" });
+            _listaPersonas.Add(new Persona() {Id = 2, Nombre = "José", Apellido = "Mosley" });
+            _listaPersonas.Add(new Persona() {Id = 3, Nombre = "Juanita", Apellido = "Diaz" });
+            _listaPersonas.Add(new Persona() {Id = 4, Nombre = "Belara", Apellido = "Dupler" });
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            string[] arrRetValues = null;
+            if (arrRetValues.Length > 0)
+            { }
+            return arrRetValues;
+        }
+
+        //[HttpGet]
+        //[Produces("application/xml")]
+        ////[Authorize]
+
+        //public List<Persona> ListarTodas()
+        //{
+        //    return _listaPersonas;
+        //}
+
+        [HttpGet("{id}")]
+        public ActionResult<Persona> ObtenerPersonaPorId(int id)
+        {
+            var persona = _listaPersonas.FirstOrDefault(p => p.Id == id);
+
+            if (persona == null)
+            {
+                return NotFound();
+            }
+
+            return persona;
+        }
+    }
+}
